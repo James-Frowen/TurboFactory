40ae72badc64f3b1fb4f3474b0c63db2a6a2c52f
JamesFrowen-PC\JamesFrowen <jamesfrowendev@gmail.com>
2018-11-07 17:00:00 +0000

    adding locale for other new objects

--- data.lua
+++ data.lua
@@ -1,3 +1,9 @@
 --data.lua
 
+-- require("prototypes.assembling")
+-- require("prototypes.belts")
+-- require("prototypes.chemical")
+-- require("prototypes.furnace")
 require("prototypes.mining")
+-- require("prototypes.pumpjack")
+-- require("prototypes.refinery")
